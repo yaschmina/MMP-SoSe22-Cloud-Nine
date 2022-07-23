5db5356d40d8afb3eca3d0b31bc92d6a062343f7
TeRadler <108184878+TeRadler@users.noreply.github.com>
2022-07-23 19:14:04 +0200

    finished setting up level2

--- Assets/Scripts/angryCloudBehaviour.cs
+++ Assets/Scripts/angryCloudBehaviour.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class angryCloudBehaviour : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
-    private Vector2 cloudStart;
+    //private Vector2 cloudStart;
     [SerializeField] private Rigidbody2D player;
     [SerializeField] private float speed = 2f;
+    private bool playerIsClose = false;
 
 
     void Start()
     {
-        //rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.position, Time.deltaTime * speed);
+        if (player.position.x > (rigidbody.position.x - 5f) && player.position.x < rigidbody.position.x + 5f)
+        {
+            playerIsClose = true;
+            //Debug.Log("true x pos player: " + player.position.x);
+            //Debug.Log("true x pos cloud: " + rigidbody.position.x);
+        }
+
+        if (playerIsClose)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, Time.deltaTime * speed);
+        }
     }
 }
